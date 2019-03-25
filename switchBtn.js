7aee37f2f63b6e140d6e175742fe151742b4769f
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-03-25 15:34:57 +0100

    add web storage

--- switchBtn.js
+++ switchBtn.js
@@ -1,11 +1,34 @@
-let body = document.querySelector('body')
-let switchBtn = document.querySelector('.container__toggle-btn')
+    // Toggle Switch
+    let body = document.querySelector('body')
+    let switchBtn = document.querySelector('.container__toggle-btn')
 
-let switchTheme = function() {
-    this.classList.toggle('active')
-    body.classList.toggle('light')
-    body.classList.toggle('dark')
+    // Web Storage
+    if (!localStorage.getItem('theme')) {
+        populateStorage()
+    } else {
+        setStyle()
+    }
 
-}
+    // Change the stat of the button and the theme of the page
+    switchBtn.addEventListener('click', switchTheme)
 
-switchBtn.addEventListener('click', switchTheme)
+    function switchTheme() {
+        body.classList.toggle('light')
+        body.classList.toggle('dark')
+        populateStorage()
+    }
+
+    function populateStorage() {
+        localStorage.setItem('theme', body.classList.value)
+
+        setStyle()
+    }
+
+    function setStyle() {
+        let currentTheme = localStorage.getItem('theme')
+        body.classList.value = currentTheme
+        if (body.classList.value === 'light')
+            switchBtn.classList.add('active')
+        else
+            switchBtn.classList.remove('active')
+    }
